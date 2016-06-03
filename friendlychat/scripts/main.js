3844dd71dea3518a0431dd6971c01a803a297b43
jaythaceo <jaythaceo@gmail.com>
2016-06-03 00:26:19 -0400

    [chore] Firebase SDK check if setup & config correctly

--- friendlychat/scripts/main.js
+++ friendlychat/scripts/main.js
@@ -222,6 +222,25 @@ FriendlyChat.prototype.toggleButton = function() {
 	}
 };
 
+// Checks that the Firebase SDK has been correctly setup and configured.
+FriendlyChat.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions.');
+  } else if (config.storageBucket === '') {
+    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
+        'actually a Firebase bug that occurs rarely.' +
+        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+        'and make sure the storageBucket attribute is not empty.');
+  }
+};
+
+window.onload = function() {
+	window.friendlyChat = new FriendlyChat();
+};
+
+
+
 
 
 
